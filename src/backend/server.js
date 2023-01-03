21d6c214f27b952e7d2ef0b9f8c26b77b541d1a6
Bond <Bondyan97@gmail.com>
2023-01-03 02:44:59 -0500

    backend route changes

--- src/backend/server.js
+++ src/backend/server.js
@@ -54,12 +54,12 @@ if (env === "production") app.use(express.static("build"));
 // END OF EXPRESS USE
 
 //ROUTES
-app.use("/api/teams", teamRoutes);
-app.use("/api/reports", reportRoutes);
-app.use("/api/matches", matchRoutes);
-app.use("/api/judges", judgeRoutes);
-app.use("/api/divisions", divisionRoutes);
-app.delete("/api/db/dropall", (req, res) => {
+app.use("/teams", teamRoutes);
+app.use("/reports", reportRoutes);
+app.use("/matches", matchRoutes);
+app.use("/judges", judgeRoutes);
+app.use("/divisions", divisionRoutes);
+app.delete("/db/dropall", (req, res) => {
   mongoose
     .createConnection(process.env.mongourl)
     .dropDatabase((err, results) => {
@@ -70,7 +70,7 @@ app.delete("/api/db/dropall", (req, res) => {
       return res.status(201).json({ success: { message: "Cleared database" } });
     });
 });
-app.get("/api/auth", (req, res) => {
+app.get("/auth", (req, res) => {
   if (!process.env.frontendauth)
     return res
       .status(500)
