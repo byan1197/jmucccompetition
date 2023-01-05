9d003b8e42410ed0b03004705259676a10fde4ed
Bond <Bondyan97@gmail.com>
2023-01-05 01:43:23 -0500

    server routing

--- src/backend/server.js
+++ src/backend/server.js
@@ -54,12 +54,12 @@ if (env === "production") app.use(express.static("build"));
 // END OF EXPRESS USE
 
 //ROUTES
-app.use("/teams", teamRoutes);
-app.use("/reports", reportRoutes);
-app.use("/matches", matchRoutes);
-app.use("/judges", judgeRoutes);
-app.use("/divisions", divisionRoutes);
-app.delete("/db/dropall", (req, res) => {
+app.use("/api/teams", teamRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/matches", matchRoutes);
+app.use("/api/judges", judgeRoutes);
+app.use("/api/divisions", divisionRoutes);
+app.delete("/api/db/dropall", (req, res) => {
   mongoose
     .createConnection(process.env.mongourl)
     .dropDatabase((err, results) => {
